b1840e7ef54bb1b8f714c45507c45f492e4b7b61
shubhampatel20 <161365957+shubhampatel20@users.noreply.github.com>
2024-10-11 11:03:35 +0000

    Pending changes exported from your codespace

---
+++ basic2/01_Arrays.js
@@ -0,0 +1,41 @@
+// const myArr = [0,2,3,4,5,6]
+
+// console.log(myArr);
+
+
+// array in javascript share shallow copy (references)
+
+
+// Deep copy => value
+ 
+
+// Array Methods
+
+//push
+//pop
+
+//myArr.unshift(9)// insert in first place
+//myArr.shift()
+ 
+// includes give boolean Result 
+ 
+// indexOf
+
+// const newArr = myArr.join() // convert array into strin
+// console.log(myArr);
+// console.log(typeof newArr);
+
+
+// slice give a part  array
+// splice manipulate array also the portion is deleted from original array
+
+
+const marvelHeros = ["thor","ironman","hulk"]
+const dcHeros = ["flash","superman"]
+
+marvelHeros.push(dcHeros)
+
+// console.log(marvelHeros);
+// console.log(marvelHeros[3][1]);
+
+
