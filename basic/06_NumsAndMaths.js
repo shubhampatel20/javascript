8e8c5d9719787aab345743c2c04ed2f7fbe95c73
shubhampatel20 <161365957+shubhampatel20@users.noreply.github.com>
2024-10-11 06:19:08 +0000

    06_NumsAndMaths

---
+++ basic/06_NumsAndMaths.js
@@ -0,0 +1,37 @@
+const score = 400
+
+const balance = new Number(100)
+
+console.log(score);
+console.log(balance);
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2)); // add decimal and two  00 output is 100.00
+
+const num = 23.87548
+console.log(num.toPrecision(3));// pricise 3 values afetr decimal
+
+const hundred= 10000000
+console.log(hundred.toLocaleString('en-IN'))
+
+
+
+//____________________MATHS______________//
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+
+// round
+// ceil
+// floor
+ 
+console.log(Math.min(2,5,1,6));// same Max 
+
+console.log(Math.random()); //by default gives random values between 0 to 1
+console.log(Math.floor(Math.random() * ));
+
+
+
+
+
