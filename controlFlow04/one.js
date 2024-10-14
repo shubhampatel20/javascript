d9c8f25eeeed2a78fd91f568b7d9682bb43a20c5
shubhampatel20 <161365957+shubhampatel20@users.noreply.github.com>
2024-10-14 13:44:05 +0000

    .

---
+++ controlFlow04/one.js
@@ -0,0 +1,42 @@
+//operators
+// <,>,=>,=<,= assign,== check equal,!= not equal, === chexk data type also 
+
+// if(2=="2"){
+//     console.log("executed");
+    
+// }
+// if(2==="2"){
+//     console.log("executed");
+    
+// }
+
+// sort hand notation 
+// const balance =1000
+
+// if(balance >100) console.log("true");
+
+
+const userLoggedIn = true
+const debitCard = true
+
+if( userLoggedIn && debitCard) {
+    console.log(" allow to purchase course");
+    
+}
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+/// truthy and falsey value 
+
+
+// falsy value
+//false, 0,-0,BigInt 0n,"",null, undefined,NaN
+
+// truthy values
+// "0",'false'," ",[],{},function(){}
